6e420b0d7c1f1582dd47ef4187ec5a49cfac9215
Roman <romanbychko84@gmail.com>
2024-07-18 18:47:03 +0300

    reading file inside a thread

--- task-3/Project.cpp
+++ task-3/Project.cpp
@@ -32,21 +32,22 @@ void Project::AnalyzeFiles() {
     for (std::uint32_t i = 0;
          const auto& file : m_files) 
          {
-        std::unique_ptr<std::ifstream> ifs = std::make_unique<std::ifstream>(file);
-        if (!ifs->is_open()) {
-            std::cerr << "Failed to open file: " << file << '\n';
-            continue;
-        }
         
-        threads[i] = std::thread([this, ifs = std::move(ifs)]() 
+        threads[i] = std::thread([this, &file]() 
         {
+            std::ifstream ifs(file);
+            if (!ifs.is_open()) {
+                std::lock_guard<std::mutex> lock(m_mutex);
+                std::cerr << "Failed to open file: " << file << '\n';
+                return;
+            }
             std::uint32_t empty_lines = 0;
             std::uint32_t comment_lines = 0;
             std::uint32_t code_lines = 0;
 
             std::string line;
             bool is_comment = false;
-            while (std::getline(*ifs, line)) {
+            while (std::getline(ifs, line)) {
                 if (line.find_first_not_of(" \t\n\v\f\r") == std::string::npos) {
                     empty_lines++;
                 } else {
@@ -78,6 +79,8 @@ void Project::AnalyzeFiles() {
     for (auto& thread : threads) 
         thread.join();
 }
+
+
 
 void Project::PrintResults() const {
     std::cout << "Total files: " << m_files.size() << '\n';
